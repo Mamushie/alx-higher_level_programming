67150f61eec2e9b963f3805341a8490c5c2ea277
Mamushie <wawerumushie@gmail.com>
2021-12-06 22:50:38 +0300

    Divide 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,11 +2,9 @@
 def safe_print_division(a, b):
     div = a / b
     try:
-        print("{} / {}= {}".format('a', 'b', 'div', end="")
-            
+        div = a / b
     except ZeroDivisionError:
-        print("You cannot divide by zero")
         div = None
-    finally: 
+    finally:
         print("Inside result: {:d}".format(div))
-        return div
+    return div
