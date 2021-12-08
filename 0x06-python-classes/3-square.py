3cfb30366fd8c30cbc0865943581fb519a0ff780
Mamushie <wawerumushie@gmail.com>
2021-12-08 11:20:23 +0300

    Write a class that defines a square by private and public attributes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defining a Class called Square"""
+
+
+class Square:
+    """Attributes of the class Square defined"""
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    def area(self):
+        """Calculates area of square"""
+        return self.__size * self.__size
