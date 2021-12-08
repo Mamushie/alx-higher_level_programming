ad2b699b9dd3458b0eb76f653e3b6ec98078e185
Wa_Mushie <85708272+Mamushie@users.noreply.github.com>
2021-12-08 11:35:04 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,6 @@
 class Square:
     """Attributes of the class Square defined"""
     def __init__(self, size=0):
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
