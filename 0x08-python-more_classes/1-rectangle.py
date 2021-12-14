96e8488c3d23c7cbfb734fa2c785c96419be301b
Mamushie <wawerumushie@gmail.com>
2021-12-14 16:30:58 +0300

    Write a class Rectangle that defines a rectangle by its attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module has a class Rectangle with attributes
+   for various calculations"""
+
+
+class Rectangle:
+    """This class has private instances for height and width
+    and decorators"""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
